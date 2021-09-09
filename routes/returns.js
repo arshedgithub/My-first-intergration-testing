e0da9ffced5db00ac7186b87aa804de1582b12e5
arshedgithub <arshedahmedmail@gmail.com>
2021-09-09 22:22:12 +0530

    prepare app for the production

--- routes/returns.js
+++ routes/returns.js
@@ -1,5 +1,4 @@
 const Joi = require("joi");
-const moment = require("moment");
 const { Rental } = require("./../models/rental");
 const { Movie } = require("../models/movie");
 const express = require("express");
@@ -8,23 +7,19 @@ const validate = require("../middleware/validate");
 const router = express.Router();
 
 router.post("/", [auth, validate(validateReturn)], async (req, res) => {
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
+
   if (!rental) return res.status(404).send("Rental not found.");
 
   if (rental.dateReturned)
     return res.status(400).send("Return already processed.");
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, "days");
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+  rental.return();
   await rental.save();
 
   await Movie.update({ _id: rental.movie._id }, { $inc: { numberInStock: 1 } });
 
-  return res.status(200).send(rental);
+  return res.send(rental);
 });
 
 function validateReturn(req) {
